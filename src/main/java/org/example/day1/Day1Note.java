7f6597d0207282a6bb51bfd858aada21e67aec0f
pisoc912 <123050861+pisoc912@users.noreply.github.com>
2024-07-19 13:33:13 -0700

    first commit

---
+++ src/main/java/org/example/day1/Day1Note.java
@@ -0,0 +1,12 @@
+package org.example.day1;
+
+public class Day1Note {
+
+    public static void main(String[] args){
+        /**
+         1. maven
+            folder structure
+            remote dependency/local dependency/ central dependency
+         */
+    }
+}
